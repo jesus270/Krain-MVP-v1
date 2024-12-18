7aa54130878459c57a7db01f81413c0b100a1870
Matt Whitney <matt@metatech.dev>
2024-12-18 10:08:02 -0700

    refactor(actions): Remove unnecessary code from referral actions
    Removed redundant code related to querying referrals.

--- src/actions/referral.ts
+++ src/actions/referral.ts
@@ -1,21 +1,5 @@
 import { db } from "@/db";
-import { and, eq } from "drizzle-orm";
 import { referralTable } from "@/db/schema";
-
-export async function getReferral({
-  referredByCode,
-  referredWalletAddress,
-}: {
-  referredByCode: string;
-  referredWalletAddress: string;
-}) {
-  return await db.query.referralTable.findFirst({
-    where: and(
-      eq(referralTable.referredByCode, referredByCode),
-      eq(referralTable.referredWalletAddress, referredWalletAddress)
-    ),
-  });
-}
 
 export async function createReferral({
   referredByCode,
